bba77c55c81c4e3c169c17dd4970c697c43dfd68
kenneth-onulak <69739963+kenneth-onulak@users.noreply.github.com>
2024-07-11 21:40:59 -0700

    CMake build recipe created.
    Add the basic entry point for testing and running Yahtzee. Included a .clang-format file for auto formatting

---
+++ include/main.cpp
@@ -0,0 +1,15 @@
+/******************************************************************************/
+/*!
+\file   main.cpp
+\author Kenneth Onulak
+\par    Yahtzee
+\date   07/11/2024
+\brief
+    Entry point for testing and running Yahtzee.
+*/
+/******************************************************************************/
+
+int main(/*int argc, char** argv*/)
+{
+    return 0;
+}
