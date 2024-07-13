f3b8757170c0c29bb085e96b3f5c7d9fbbfe972d
kenneth-onulak <69739963+kenneth-onulak@users.noreply.github.com>
2024-07-12 22:38:05 -0700

    Tested dice to ensure random number generation creates expected values

--- include/main.cpp
+++ include/main.cpp
@@ -9,7 +9,40 @@
 */
 /******************************************************************************/
 
+#include "D6.h"
+#include <unordered_set>
+#include <iostream>
+#include <string>
+#include <vector>
+
+// Verify random number produces all-inclusive values between 1-6
+void test0()
+{
+    std::unordered_set<int> generatedNumbers; // Store unique values
+    D6 dice;                                  // Random number generator
+
+    // Test the dice for all values
+    for (int i = 0; i < 100; ++i)
+    {
+        dice.roll();
+        generatedNumbers.insert(dice.getValue());
+        std::cout << dice.getValue() << std::endl;
+
+        // Break if all numbers have appeared at least once
+        if (generatedNumbers.size() == 6)
+        {
+            break;
+        }
+    }
+}
+
 int main(/*int argc, char** argv*/)
 {
+    // Vector of function pointers
+    std::vector<void(*)()> pTests = { test0 };
+    int test = 0;
+
+    pTests[test]();
+
     return 0;
 }
